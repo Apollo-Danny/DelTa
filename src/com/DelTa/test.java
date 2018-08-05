762bd6068d8ba18f52116f331ab87110be053c1f
Tewei123456 <renhengkai@trawe.cn>
2018-08-05 12:53:14 +0800

    'update'

---
+++ src/com/DelTa/test.java
@@ -0,0 +1,34 @@
+package com.DelTa;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ren
+ * Date: 2018/8/5
+ * Time: 12:44
+ */
+public class test {
+
+
+
+    private String name ;
+
+
+
+    private String number ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
